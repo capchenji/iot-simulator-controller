e4ae5826dbd7d2d2aec1d6f6e1fb0e2f78965180
Ji <ji@Jis-MacBook-Pro.local>
2020-03-24 21:03:12 +0800

    add controller folder inside

---
+++ api-controller/iot-generator-controller/handler.js
@@ -0,0 +1,35 @@
+'use strict';
+var mysql = require('mysql');
+
+
+module.exports.hello = async (event) => {
+  
+  var connection = mysql.createConnection({
+    host     : process.env.RDS_HOSTNAME,
+    user     : process.env.RDS_USERNAME,
+    password : process.env.RDS_PASSWORD,
+    port     : process.env.RDS_PORT
+  });
+  
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
+  
+  connection.end();
+  
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
